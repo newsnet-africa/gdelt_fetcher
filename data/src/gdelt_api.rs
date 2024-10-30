9d3a2cfa2180a537eee4ce38c8bd944f6de2dfe3
Nzuzo <nzuzo@newsnet.africa>
2024-10-30 18:01:18 +0200

    config

--- data/src/gdelt_api.rs
+++ data/src/gdelt_api.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Duration, Utc};
 use iso_country::Country;
+use crate::utils::types::api_types::*;
 
 pub struct GdeltApiRequest {
     query: Vec<QueryType>,
@@ -14,42 +15,19 @@ pub struct GdeltApiRequest {
     timezoom: bool
 }
 
-enum QueryType {
-    Quote(String),
-    BooleanOR(Vec<String>),
-    Exclude(Vec<QueryType>),
-    Domain(String),
-    DomainIs(String),
-    ImageFaceTone(Operation, f16),
-    ImageNumFaces(Operation, u8),
-    ImageORCMeta(String),
-    ImageTags(Vec<String>),
-    ImageWebCount(Operation, u128),
-    Near {
-        distance: u8,
-        root_word: String,
-        near_word: String,
-    },
-    Repeat(String),
-    SourceCountry(Country),
-    SourceLang(Language),
-    Theme(GKGTheme),
-    Tone(Operation, f16),
-    ToneAbs(Operation, f16),
-}
-
-enum Mode {
-    ArtList,
-    ArtGallery,
-    ImageCollage,
-    ImageCollageInfo,
-    ImageGallery,
-    ImageCollageShare,
-    TimelineVol,
-    TimelineVolRow,
-    TimelineVolInfo,
-    TimelineTone,
-    TimelineLang,
-    TimelineSourceCountry,
-    // TODO: The rest of these
+impl GdeltApiRequest {
+    pub fn new(query: QueryType, mode: OutputMode, format: OutputFormat) {
+        Self {
+            query,
+            mode,
+            format,
+            timestamp: None,
+            start_end_times: None,
+            max_records: None,
+            timeline: None,
+            trans: None,
+            sort: None,
+            timezoom: false,
+        }
+    }
 }
--- data/src/utils/types.rs
+++ data/src/utils/types.rs
@@ -27,3 +27,74 @@ pub enum JsonRustTypes {
     /// Array JSON type.
     Array,
 }
+
+pub mod api_types {
+    pub enum QueryType {
+        Quote(String),
+        BooleanOR(Vec<String>),
+        Exclude(Vec<QueryType>),
+        Domain(String),
+        DomainIs(String),
+        ImageFaceTone(Operation, f16),
+        ImageNumFaces(Operation, u8),
+        ImageORCMeta(String),
+        ImageTags(Vec<String>),
+        ImageWebCount(Operation, u128),
+        Near {
+            distance: u8,
+            root_word: String,
+            near_word: String,
+        },
+        Repeat(String),
+        SourceCountry(Country),
+        SourceLang(Language),
+        Theme(GKGTheme),
+        Tone(Operation, f16),
+        ToneAbs(Operation, f16),
+    }
+
+    pub enum OutputMode {
+        ArtList,
+        ArtGallery,
+        ImageCollage,
+        ImageCollageInfo,
+        ImageGallery,
+        ImageCollageShare,
+        TimelineVol,
+        TimelineVolRow,
+        TimelineVolInfo,
+        TimelineTone,
+        TimelineLang,
+        TimelineSourceCountry,
+        ToneChart,
+        WordCloudImageTags,
+        WordCloudImageWebTags
+    }
+
+    pub enum OutputFormat {
+        Html,
+        CSV,
+        RSS(RSSType),
+        JSON(JSONType),
+    }
+
+    pub enum RSSType {
+        Default,
+        Archive,
+    }
+
+    pub enum JSONType {
+        Callback(String),
+        Feed,
+    }
+
+    pub enum Translator {
+        Google
+    }
+
+    pub enum SortType {
+        Date(bool),
+        Tone(bool),
+        HybridRel,
+    }
+}
